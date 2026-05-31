c5a0f48f3bde5b397e1d80fa55aacaa04c5499dc
derey79 <devyreymond@gmail.com>
2026-05-31 23:38:14 +0700

    update function to fecth data, getPopular and getByID

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,6 +4,30 @@ import api from '@/lib/axios';
 // TODO: Create service functions to fetch data from TMDB API
 // Reference: https://developer.themoviedb.org/reference/intro/getting-started
 
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+  genre_ids: number[];
+}
+
+export interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface CreateMovieInput {
+  title: string;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+}
+
 export const movieService = {
   // TODO: Implement getPopularMovies function
   // Endpoint: GET /movie/popular
@@ -18,4 +42,22 @@ export const movieService = {
   // Endpoint: GET /search/movie
 
   // TODO: Add more endpoints as needed
+
+  getPopular: async (page = 1): Promise<MovieResponse> => {
+    return api.get('/movie/popular', { params: { page } });
+  },
+
+  /**
+   * TMDB movie details endpoint: /movie/{id}
+   */
+  getById: async (id: number | string): Promise<Movie> => {
+    return api.get(`/movie/${id}`);
+  },
+
+  /**
+   * TMDB search endpoint pattern is: /search/movie
+   */
+  search: async (query: string, page = 1): Promise<MovieResponse> => {
+    return api.get('/search/movie', { params: { query, page } });
+  },
 };
